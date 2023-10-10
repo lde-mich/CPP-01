66aaac6297161213ec9f91f624809ce76772f057
Luca De Micheli <lde-mich@e4r5p2.42roma.it>
2023-10-10 17:17:20 +0200

    update-oct-10

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,18 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:47:53 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/10 15:47:54 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/10/10 17:16:11 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <iostream>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        void Announce();
+        Zombie* newZombie(std::string name);
+};
