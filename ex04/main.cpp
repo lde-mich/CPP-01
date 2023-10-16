77d9d7c92b9cee4d018936834d143ea390827293
Luca De Micheli <lde-mich@e4r6p2.42roma.it>
2023-10-16 18:42:07 +0200

    update-oct-16

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,19 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:05:59 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/16 18:34:50 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:41:48 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "replace.hpp"
+#include <iostream>
+#include <unistd.h>
 
 int main (int argc, char **argv)
 {
+    std::string s1;
+    std::string s2;
+
+    if (argc != 3)
+        return(write(2, "Error:\n Bad arguments\n", 22));
     return (0);
 }
