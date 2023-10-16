68dde5765f798cc9a05b54c85e15ea0139e891b1
Luca De Micheli <lde-mich@e4r6p2.42roma.it>
2023-10-16 18:35:34 +0200

    update-oct-16

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,13 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:05:59 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/16 17:06:31 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:34:50 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "replace.hpp"
 
-int main ()
+int main (int argc, char **argv)
 {
     return (0);
 }
